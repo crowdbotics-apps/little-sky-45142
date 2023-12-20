abe347cad7e5b497170f066dad248bf3718fa52b
Crowdbotics <team@crowdbotics.com>
2023-12-20 21:23:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-sky-45142",
+  url: "https://little-sky-45142.botics.co",
+  api: "https://little-sky-45142.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
